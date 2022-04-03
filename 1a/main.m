4c4ed3ae8854208b71d72987b7bb0b5c1dbddb35
Zygo01 <kjagla0@gmail.com>
2022-04-03 23:38:20 +0200

    some work

--- 1a/main.m
+++ 1a/main.m
@@ -1,8 +1,8 @@
 pkg load miscellaneous;
 M = csvread("Data/1.csv");
 f=M(2:end,1);Uwy=M(2:end,2);Uwe=M(2:end,3);
-#{
-loglog(f,Uwy,".r","markersize", 15, f,Uwy,"linewidth",2)
+
+semilogx(f,Uwy,".r","markersize", 15)
 title ("Frequency characteristics");
 xlabel ("f [Hz]");
 ylabel ("Uwy [V]");
@@ -10,7 +10,7 @@ grid minor on
 print "Uwy(f).pdf";
 close();
 
-loglog(f,Uwe,".r","markersize", 15, f,Uwe,"linewidth",2)
+semilogx(f,Uwe,".r","markersize", 15)
 title ("Dynamic characteristics");
 xlabel ("f [Hz]");
 ylabel ("Uwe [V]");
@@ -18,14 +18,14 @@ grid minor on
 print "Uwe(f).pdf";
 close();
 
-loglog(f,(Uwy./Uwe),".r","markersize", 15, f,(Uwy./Uwe),"linewidth",2)
+semilogx(f,(Uwy./Uwe),".r","markersize", 15)
 title ("Amplification characteristics");
 xlabel ("f [Hz]");
 ylabel ("Ku");
 grid minor on
 print "Ku.pdf";
 close();
-#}
+#{
 plot(f,Uwy,".r","markersize", 15, f,Uwy,"linewidth",2)
 title ("Uwy(f)");
 xlabel ("f [Hz]");
@@ -41,3 +41,4 @@ ylabel ("Uwe [V]");
 grid minor on
 print "DaneUwe.pdf";
 close();
+#}
